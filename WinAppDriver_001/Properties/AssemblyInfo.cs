4b7f1262315ef5584802730c9b78bb6a41d83165
Sohaib <k200292@nu.edu.pk>
2024-07-31 21:50:16 +0500

    first commit

---
+++ WinAppDriver_001/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinAppDriver_001")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinAppDriver_001")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52bda842-532d-4b8e-be4b-a2604c61b45b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WinAppDriver_001/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace WinAppDriver_001
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculatorAddition_TC01()
+        {
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe");
+            AppiumOptions options = new AppiumOptions();
+            options.AddAdditionalCapability("app", @"Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+
+            var driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"),options);
+            Thread.Sleep(3000);
+            driver.FindElementByName("Five").Click();
+            driver.FindElementByName("Plus").Click();
+            driver.FindElementByName("Seven").Click();
+            driver.FindElementByName("Equals").Click();
+
+            var calculatorResult = driver.FindElementByAccessibilityId("CalculatorResults").Text;
+            Assert.AreEqual("12", calculatorResult.Replace("Display is", string.Empty).Trim());
+            driver.CloseApp();
+        }
+        [TestMethod]
+        public void CalculatorSubtraction_TC02()
+        {
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe");
+            AppiumOptions options = new AppiumOptions();
+            options.AddAdditionalCapability("app", @"Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+
+            var driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+            Thread.Sleep(3000);
+            driver.FindElementByName("Nine").Click();
+            driver.FindElementByName("Minus").Click();
+            driver.FindElementByName("Seven").Click();
+            driver.FindElementByName("Equals").Click();
+
+            var calculatorResult = driver.FindElementByAccessibilityId("CalculatorResults").Text;
+            Assert.AreEqual("2", calculatorResult.Replace("Display is", string.Empty).Trim());
+            driver.CloseApp();
+        }
+
+        [TestMethod]
+        public void CalculatorMultiplication_TC03()
+        {
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe");
+            AppiumOptions options = new AppiumOptions();
+            options.AddAdditionalCapability("app", @"Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+
+            var driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+            Thread.Sleep(3000);
+            driver.FindElementByName("Three").Click();
+            driver.FindElementByName("Multiply by").Click();
+            driver.FindElementByName("Four").Click();
+            driver.FindElementByName("Equals").Click();
+
+            var calculatorResult = driver.FindElementByAccessibilityId("CalculatorResults").Text;
+            Assert.AreEqual("12", calculatorResult.Replace("Display is", string.Empty).Trim());
+            driver.CloseApp();
+        }
+    }
+}
